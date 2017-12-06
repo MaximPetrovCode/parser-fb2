ecf494e72ac9ae0ac5eb44b6bcf5384146224477
Max <XXXXXX@gmail.com>
2017-12-06 16:28:02 +0300

    Rewrited using promises

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ const xml2js = require('xml2js');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    var parser = new xml2js.Parser();
-    
-        fs.readFile('./books/english.fb2', function(err, data) {
+function readBook() {
+    return new Promise((resolve, reject) => {
+        var parser = new xml2js.Parser();
+
+        fs.readFile('./books/english.fb2', function (err, data) {
             parser.parseString(data, function (err, result) {
                 // console.dir(result);
                 console.log('Done');
-                res.send(result);
+                let content = result.FictionBook.body[0].section[0].p;
+
+                resolve(content);
+
+                // content.forEach(element => {
+                //     console.log(element);
+
+                // });
             });
-        });    
-});
-    
-const port = 5000;
-app.listen(port, (req,res)=>{
-    console.log(port);
-});
+
+        });
+    });
+}
+
+function parseWords(content){
+    return new Promise((resolve, reject) => {
+        resolve(content);
+    });
+}
+
+readBook()
+.then(parseWords)
+.then(console.log);
+
